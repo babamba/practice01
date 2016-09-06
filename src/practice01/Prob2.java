8a66f24dd6bb3824959a9407d016e5fa7da79375
bit-user <orochi13@naver.com>
2016-09-06 17:42:16 +0900

    풀다 중단

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -4,6 +4,18 @@ package practice01;
 public class Prob2 {
 
 	public static void main(String[] args) {
+	System.out.println("실행결과" + "\n" + "ㅡㅡㅡㅡ");
+	
+	
+	for(int i = 1; i<11; i++){
+		System.out.print(i + " ");
+			
+		}
+	
+		
 	}
-
-}
+	
+	
+	
+	}
+	
