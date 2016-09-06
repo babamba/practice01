7aeffc174239ccf4be6ac9ef702002ef21d78c6a
bit-user <orochi13@naver.com>
2016-09-06 16:43:47 +0900

    과제풀어오기

--- src/practice01/Game369.java
+++ src/practice01/Game369.java
@@ -1,5 +1,17 @@
 package practice01;
 public class Game369 {
 	public static void main(String[] args) {
+		
+		int number = 36942;	//문자열로 보고 푸는것 추천
+		String s = String.valueOf(number); //number를 문자열로 변환
+		
+		System.out.println(s.length());
+		char c = s.charAt(2); //0부터 시작해서 2번째 문자를 빼내라
+		System.out.println(c);
+		
+		int ClapCount = 0;
+		if(c == '3' || c == '6' || c == '9'){
+			ClapCount++;
+		}
 	}
 }
--- src/practice01/GuessNumber.java
+++ src/practice01/GuessNumber.java
@@ -1,11 +1,18 @@
 package practice01;
 
+import java.util.Random;
 import java.util.Scanner;
-
+//100라인정도
+//힌트 컴퓨터가 숫자를 생각하게 하는것 
 public class GuessNumber {
 	public static void main(String[] args) {
+		Random r = new Random();
+		int guessNumber = r.nextInt(100) + 1;
+		
+		System.out.println(guessNumber);
 		
 		Scanner scanner = new Scanner( System.in );
 		scanner.close();
 	}
+//do while or while break
 }
