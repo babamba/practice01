171bf1a7463cd0b3b69d7cc282c1cb030355399a
rlawlsdnjs <orochi13@naver.com>
2016-09-07 16:51:50 +0900

    수정

--- src/practice01/Prob2.java
+++ src/practice01/Prob2.java
@@ -5,17 +5,13 @@ public class Prob2 {
 
 	public static void main(String[] args) {
 	System.out.println("실행결과" + "\n" + "ㅡㅡㅡㅡ");
-	
-	
-	for(int i = 1; i<11; i++){
-		System.out.print(i + " ");
+
+	for(int j = 1; j <= 8; j++){
+		for(int i = j; i <= j+9; i++ ){
+			System.out.print(i + " ");
+				}
+				System.out.println();
+			}
 			
-		}
-	
-		
+	  	}
 	}
-	
-	
-	
-	}
-	
